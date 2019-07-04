225a44dedd9700b89fdc287ad78ebebee557a2dd
Lu-RH <497824490@qq.com>
2019-07-04 15:42:06 +0800

    添加docker-php依赖

--- src/Client.php
+++ src/Client.php
@@ -12,26 +12,26 @@ class Client
         echo 1111;
     }
 
-    // /**
-    //  * 连接docker
-    //  * @param string $remote 远程 IP:端口
-    //  * @return Docker\Docker
-    //  */
-    // public function connectDocker($remote = 0)
-    // {
-    //     //连接本地docker
-    //     if ($remote == 0) {
-    //         $docker = Docker::create();
-    //     } else {
-    //         //连接远程节点
-    //         $client = DockerClientFactory::create([
-    //             'remote_socket' => 'tcp://' . $remote,
-    //         ]);
-    //         $docker = Docker::create($client);
-    //     }
-    //     $this->docker = $docker;
-    //     return $docker;
-    // }
+    /**
+     * 连接docker
+     * @param string $remote 远程 IP:端口
+     * @return Docker\Docker
+     */
+    public function connectDocker($remote = 0)
+    {
+        //连接本地docker
+        if ($remote == 0) {
+            $docker = Docker::create();
+        } else {
+            //连接远程节点
+            $client = DockerClientFactory::create([
+                'remote_socket' => 'tcp://' . $remote,
+            ]);
+            $docker = Docker::create($client);
+        }
+        $this->docker = $docker;
+        return $docker;
+    }
 }
 
 
