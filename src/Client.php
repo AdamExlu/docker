3cc1f73a25c81e94de7f1cb9d4e41db1d93a6d82
Lu-RH <497824490@qq.com>
2019-07-04 15:20:04 +0800

    测试

--- src/Client.php
+++ src/Client.php
@@ -1,12 +1,37 @@
 <?php
 namespace lrh\docker;
+// use Docker\Docker;
+// use Docker\DockerClientFactory;
 
 class Client 
 {
+    public $docker;
+
     static public function test()
     {
         echo 1111;
     }
+
+    // /**
+    //  * 连接docker
+    //  * @param string $remote 远程 IP:端口
+    //  * @return Docker\Docker
+    //  */
+    // public function connectDocker($remote = 0)
+    // {
+    //     //连接本地docker
+    //     if ($remote == 0) {
+    //         $docker = Docker::create();
+    //     } else {
+    //         //连接远程节点
+    //         $client = DockerClientFactory::create([
+    //             'remote_socket' => 'tcp://' . $remote,
+    //         ]);
+    //         $docker = Docker::create($client);
+    //     }
+    //     $this->docker = $docker;
+    //     return $docker;
+    // }
 }
 
 
